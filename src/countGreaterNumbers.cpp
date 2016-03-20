37ef8655b8a39abf4d2b8bf1b8d8b072ba98452d
chandu54 <chandu54@missionrnd.com>
2016-03-20 19:40:49 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,13 +13,60 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
+#include<malloc.h>
 
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
-
+struct dates
+{
+	int dd, mm, yyyy;
+};
+struct dates function(char* strdate)
+{
+	struct dates intdate;
+	intdate.dd = 0;
+	intdate.mm = 0;
+	intdate.yyyy = 0;
+	int index = 0;
+	if (strdate[2] == '-'&&strdate[5] == '-')
+	{
+		for (index = 0; index<2; index++)
+			intdate.dd = ((intdate.dd) * 10) + (strdate[index] - '0');
+		for (index = 3; index<5; index++)
+			intdate.mm = ((intdate.mm) * 10) + (strdate[index] - '0');
+		for (index = 6; index<10; index++)
+			intdate.yyyy = ((intdate.yyyy) * 10) + (strdate[index] - '0');
+	}
+	//printf("%d %d %d\n ",intdate.dd,intdate.mm,intdate.yyyy);
+	return intdate;
+}
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	int i, count = 0;
+	struct dates intdate[5];
+	struct dates givendate;
+	givendate = function(date);
+	//printf("\n %d %d %d\n ",givendate.dd,givendate.mm,givendate.yyyy);
+	for (i = 0; i<len; i++)
+		intdate[i] = function(Arr[i].date);
+	for (i = 0; i<len; i++)
+		//printf("\n %d %d %d",intdate[i].dd,intdate[i].mm,intdate[i].yyyy);
+		for (i = 0; i<len; i++)
+		{
+			if (intdate[i].yyyy>givendate.yyyy)
+				count++;
+			else if (intdate[i].yyyy == givendate.yyyy)
+			{
+				if (intdate[i].mm>givendate.mm)
+					count++;
+				else if (intdate[i].mm == givendate.mm)
+				{
+					if (intdate[i].dd>givendate.dd)
+						count++;
+				}
+			}
+		}
+	return count;
 }
